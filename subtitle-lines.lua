b3cf1f1f1260ffabddd824a40f2ad64697362d82
Christoph Heinrich <christoph.heinrich@student.tugraz.at>
2023-11-09 03:14:29 +0100

    Fix crash when no file loaded

--- subtitle-lines.lua
+++ subtitle-lines.lua
@@ -166,7 +166,7 @@ local function show_subtitle_list(subtitles)
 
     local last_started_index = 0
     local last_active_index = nil
-    local time = mp.get_property_number('time-pos') + SUB_SEEK_OFFSET
+    local time = mp.get_property_number('time-pos', 0) + SUB_SEEK_OFFSET
     for i, subtitle in ipairs(subtitles) do
         local has_started = subtitle.start <= time
         local has_ended = subtitle.stop < time
